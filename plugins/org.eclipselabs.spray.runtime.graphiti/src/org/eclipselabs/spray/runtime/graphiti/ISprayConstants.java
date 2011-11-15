af1ec6a340661a5a649546b03a11a6b06ef54cc3
Karsten Thoms <karsten.thoms@itemis.de>
2011-11-15 08:31:04 +0100

    adding PROPERTY_URI
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- plugins/org.eclipselabs.spray.runtime.graphiti/src/org/eclipselabs/spray/runtime/graphiti/ISprayConstants.java
+++ plugins/org.eclipselabs.spray.runtime.graphiti/src/org/eclipselabs/spray/runtime/graphiti/ISprayConstants.java
@@ -4,4 +4,8 @@ public interface ISprayConstants {
     // KEYS used for IPeService#setPropertyValue()
     String PROPERTY_MODEL_TYPE = "MODEL_TYPE";
     String PROPERTY_STATIC     = "STATIC";
+    /**
+     * Property stores an URI string
+     */
+    String PROPERTY_URI        = "URI";
 }
