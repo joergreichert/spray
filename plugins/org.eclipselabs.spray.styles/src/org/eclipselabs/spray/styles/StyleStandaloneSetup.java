2c2291c00fdde83f4d2b32f21d841ac6f3ebcab4
Fabio Filippelli <fabio.filippelli@googlemail.com>
2011-12-09 16:03:02 +0100

    Refactored styles according to naming conventions

---
+++ plugins/org.eclipselabs.spray.styles/src/org/eclipselabs/spray/styles/StyleStandaloneSetup.java
@@ -0,0 +1,14 @@
+
+package org.eclipselabs.spray.styles;
+
+/**
+ * Initialization support for running Xtext languages 
+ * without equinox extension registry
+ */
+public class StyleStandaloneSetup extends StyleStandaloneSetupGenerated{
+
+	public static void doSetup() {
+		new StyleStandaloneSetup().createInjectorAndDoEMFRegistration();
+	}
+}
+
