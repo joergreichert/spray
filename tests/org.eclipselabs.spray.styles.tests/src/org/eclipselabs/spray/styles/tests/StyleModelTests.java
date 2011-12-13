1d928d0471cef2668c9ef1d50931aad83436f0f5
Fabio Filippelli <fabio.filippelli@googlemail.com>
2011-12-13 20:29:37 +0100

    Added test cases and model files to style project

---
+++ tests/org.eclipselabs.spray.styles.tests/src/org/eclipselabs/spray/styles/tests/StyleModelTests.java
@@ -0,0 +1,81 @@
+package org.eclipselabs.spray.styles.tests;
+
+import org.eclipse.xtext.junit4.InjectWith;
+import org.eclipselabs.spray.styles.StyleInjectorProvider;
+import org.eclipselabs.xtext.utils.unittesting.XtextRunner2;
+import org.eclipselabs.xtext.utils.unittesting.XtextTest;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(XtextRunner2.class)
+@InjectWith(StyleInjectorProvider.class)
+public class StyleModelTests extends XtextTest {
+	
+	@Before
+	public void before() {
+		super.before();
+		suppressSerialization();
+	}
+
+	@Test
+	public void test_01_FullFilledStyle() {
+		testFile("testcases/01_FullFilledStyle.style");
+	}
+
+	@Test
+	public void test_02_NoTransparency() {
+		testFile("testcases/02_NoTransparency.style");
+	}
+
+	@Test
+	public void test_03_DefaultBackground() {
+		testFile("testcases/03_DefaultBackground.style");
+	}
+
+	@Test
+	public void test_04_TransparentBackground() {
+		testFile("testcases/04_TransparentBackground.style");
+	}
+
+	@Test
+	public void test_05_NoLinecolor() {
+		testFile("testcases/05_NoLinecolor.style");
+	}
+
+	@Test
+	public void test_06_TransparentLineColor() {
+		testFile("testcases/06_TransparentLineColor.style");
+	}
+
+	@Test
+	public void test_07_NoFontname() {
+		testFile("testcases/07_NoFontname.style");
+	}
+
+	@Test
+	public void test_08_NoFontsize() {
+		testFile("testcases/08_NoFontsize.style");
+	}
+
+	@Test
+	public void test_09_NoFontnameAndSize() {
+		testFile("testcases/09_NoFontnameAndSize.style");
+	}
+
+	@Test
+	public void test_10_NoFontStyle() {
+		testFile("testcases/10_NoFontStyle.style");
+	}
+
+	@Test
+	public void test_11_EmptyStyle() {
+		testFile("testcases/11_EmptyStyle.style");
+	}
+
+	@Test
+	public void test_12_StyleInheritance() {
+		testFile("testcases/12_StyleInheritance.style");
+	}
+
+}
