b5614044e22663678929a6572c928ff918d80a23
Karsten Thoms <karsten.thoms@itemis.de>
2012-03-25 15:13:59 -0400

    added implicit imports
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- plugins/org.eclipselabs.spray.shapes/src/org/eclipselabs/spray/shapes/ShapeRuntimeModule.java
+++ plugins/org.eclipselabs.spray.shapes/src/org/eclipselabs/spray/shapes/ShapeRuntimeModule.java
@@ -4,6 +4,7 @@
 package org.eclipselabs.spray.shapes;
 
 import org.eclipse.xtext.scoping.IScopeProvider;
+import org.eclipselabs.spray.shapes.scoping.ShapeImportedNamespaceScopeProvider;
 import org.eclipselabs.spray.shapes.scoping.ShapeScopeProvider;
 
 /**
@@ -11,28 +12,26 @@ import org.eclipselabs.spray.shapes.scoping.ShapeScopeProvider;
  * Equinox extension registry.
  */
 public class ShapeRuntimeModule extends org.eclipselabs.spray.shapes.AbstractShapeRuntimeModule {
-	@Override
-	public Class<? extends IScopeProvider> bindIScopeProvider() {
-		return ShapeScopeProvider.class;
-	}
+    @Override
+    public Class<? extends IScopeProvider> bindIScopeProvider() {
+        return ShapeScopeProvider.class;
+    }
 
-	@Override
-	public Class<? extends org.eclipse.xtext.generator.IGenerator> bindIGenerator() {
-		return org.eclipselabs.spray.shapes.generator.ShapeGenerator.class;
-	}
+    @Override
+    public Class<? extends org.eclipse.xtext.generator.IGenerator> bindIGenerator() {
+        return org.eclipselabs.spray.shapes.generator.ShapeGenerator.class;
+    }
 
-	// contributed by
-	// org.eclipse.xtext.generator.scoping.AbstractScopingFragment
-	public void configureIScopeProviderDelegate(com.google.inject.Binder binder) {
-		binder.bind(org.eclipse.xtext.scoping.IScopeProvider.class)
-				.annotatedWith(com.google.inject.name.Names.named(org.eclipse.xtext.scoping.impl.AbstractDeclarativeScopeProvider.NAMED_DELEGATE))
-				.to(org.eclipse.xtext.scoping.impl.ImportedNamespaceAwareLocalScopeProvider.class);
-	}
+    // contributed by
+    // org.eclipse.xtext.generator.scoping.AbstractScopingFragment
+    public void configureIScopeProviderDelegate(com.google.inject.Binder binder) {
+        binder.bind(org.eclipse.xtext.scoping.IScopeProvider.class).annotatedWith(com.google.inject.name.Names.named(org.eclipse.xtext.scoping.impl.AbstractDeclarativeScopeProvider.NAMED_DELEGATE)).to(ShapeImportedNamespaceScopeProvider.class);
+    }
 
-	// contributed by
-	// org.eclipse.xtext.generator.exporting.QualifiedNamesFragment
-	public Class<? extends org.eclipse.xtext.naming.IQualifiedNameProvider> bindIQualifiedNameProvider() {
-		return org.eclipse.xtext.naming.DefaultDeclarativeQualifiedNameProvider.class;
-	}
+    // contributed by
+    // org.eclipse.xtext.generator.exporting.QualifiedNamesFragment
+    public Class<? extends org.eclipse.xtext.naming.IQualifiedNameProvider> bindIQualifiedNameProvider() {
+        return org.eclipse.xtext.naming.DefaultDeclarativeQualifiedNameProvider.class;
+    }
 
 }
