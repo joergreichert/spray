7a335848791efa1744e9b6422a50ef6ad485f278
Karsten Thoms <karsten.thoms@itemis.de>
2012-04-02 00:19:39 +0200

    fixed formatting problem for 'style' keyword
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- plugins/org.eclipselabs.spray.shapes/src/org/eclipselabs/spray/shapes/formatting/ShapeFormatter.java
+++ plugins/org.eclipselabs.spray.shapes/src/org/eclipselabs/spray/shapes/formatting/ShapeFormatter.java
@@ -71,9 +71,10 @@ public class ShapeFormatter extends AbstractDeclarativeFormatter {
         c.setLinewrap().before(grammar.getTextLayoutAccess().getAlignKeyword_1_1_0());
         c.setLinewrap().before(grammar.getTextBodyAccess().getIdKeyword_0());
         c.setLinewrap().before(grammar.getShapestyleLayoutAccess().getStyleKeyword_1_0());
-        for (Keyword kw : grammar.findKeywords("line", "ellipse", "rectangle", "rounded-rectangle", "polyline", "polygon", "text", "align", "id", "style")) {
+        for (Keyword kw : grammar.findKeywords("line", "ellipse", "rectangle", "rounded-rectangle", "polyline", "polygon", "text", "align", "id")) {
             c.setLinewrap().before(kw);
         }
+
         c.setLinewrap().before(grammar.getCDTextRule());
     }
 
