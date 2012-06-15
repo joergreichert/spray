d43cbccc54dbc270fa82e8254f12f6dfee3d21ed
Joerg Reichert <joerg.reichert@itemis.de>
2012-06-15 07:04:36 +0200

    fixed org.eclipselabs.spray.xtext.tests

--- tests/org.eclipselabs.spray.xtext.tests/src/org/eclipselabs/spray/xtext/tests/ValidationRulesTest.java
+++ tests/org.eclipselabs.spray.xtext.tests/src/org/eclipselabs/spray/xtext/tests/ValidationRulesTest.java
@@ -1,5 +1,9 @@
 package org.eclipselabs.spray.xtext.tests;
 
+import org.eclipse.emf.codegen.ecore.genmodel.GenModelPackage;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.plugin.EcorePlugin;
 import org.eclipse.xtext.junit4.InjectWith;
 import org.eclipselabs.spray.xtext.SprayTestsInjectorProvider;
 import org.eclipselabs.xtext.utils.unittesting.XtextRunner2;
@@ -9,6 +13,8 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import BusinessDomainDsl.IBusinessDomainDslPackage;
+
 @RunWith(XtextRunner2.class)
 @InjectWith(SprayTestsInjectorProvider.class)
 public class ValidationRulesTest extends XtextTest {
@@ -16,6 +22,10 @@ public class ValidationRulesTest extends XtextTest {
     public void before() {
         super.before();
         suppressSerialization();
+		EPackage.Registry.INSTANCE.put(IBusinessDomainDslPackage.eNS_URI, IBusinessDomainDslPackage.eINSTANCE);
+        EPackage.Registry.INSTANCE.put(GenModelPackage.eNS_URI, GenModelPackage.eINSTANCE);
+
+        EcorePlugin.getEPackageNsURIToGenModelLocationMap().put(IBusinessDomainDslPackage.eNS_URI, URI.createURI("classpath:/mod4j/BusinessDomainDsl.genmodel"));
     }
 
     @Test
@@ -40,9 +50,7 @@ public class ValidationRulesTest extends XtextTest {
     @Test
     public void test_connection_02() {
         testFile("validation/connection-02.spray", "mod4j/BusinessDomainDsl.ecore");
-        // there are warnings due to missing create behavior, ignore them
         assertConstraints(issues.errorsOnly().theOneAndOnlyContains("Couldn't resolve reference to EReference 'businessRules'"));
-        assertConstraints(issues.warningsOnly().sizeIs(2));
     }
 
     @Test
