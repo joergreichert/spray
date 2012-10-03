c743af318111c776ac21e1d836126a2e6c3ec436
Jos Warmer <joswarmer@gmail.com>
2012-10-03 22:53:53 +0200

    clesaming up

--- plugins/org.eclipselabs.spray.runtime.graphiti/src/org/eclipselabs/spray/runtime/graphiti/ISprayConstants.java
+++ plugins/org.eclipselabs.spray.runtime.graphiti/src/org/eclipselabs/spray/runtime/graphiti/ISprayConstants.java
@@ -24,7 +24,6 @@ public interface ISprayConstants {
     // CONSTANTS used by shapes
     String TEXT_ID                                   = "SHAPEDSL_TEXT_ID";
     String IS_SHAPE_FROM_DSL                         = "IS_SHAPE_FROM_DSL";
-    String IS_SHAPE_FROM_DSL_VALUE                   = "true";
     String WIDTH_KEY                                 = "current-width-shape";
     String HEIGHT_KEY                                = "current-height-shape";
 
