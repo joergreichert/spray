143c48d9fea33fb53a1b06d2399252658562b794
Jos Warmer <joswarmer@gmail.com>
2012-11-10 18:59:00 +0100

    Fixed http://code.google.com/a/eclipselabs.org/p/spray/issues/detail?id=196 and updated documentation

--- plugins/org.eclipselabs.spray.xtext.ui/src/org/eclipselabs/spray/xtext/ui/contentassist/SprayJdtTypesProposalPriorities.java
+++ plugins/org.eclipselabs.spray.xtext.ui/src/org/eclipselabs/spray/xtext/ui/contentassist/SprayJdtTypesProposalPriorities.java
@@ -1,12 +1,16 @@
 package org.eclipselabs.spray.xtext.ui.contentassist;
 
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.xtext.ui.editor.contentassist.ConfigurableCompletionProposal;
 import org.eclipse.xtext.ui.editor.contentassist.ContentProposalPriorities;
 
 public class SprayJdtTypesProposalPriorities extends ContentProposalPriorities {
 
     @Override
     protected void adjustPriority(ICompletionProposal proposal, String prefix, int priority) {
+        if (proposal == null || !(proposal instanceof ConfigurableCompletionProposal)) {
+            return;
+        }
         if (!proposal.getDisplayString().contains(".")) {
             super.adjustPriority(proposal, prefix, priority * proposalWithPrefixMultiplier);
         }
