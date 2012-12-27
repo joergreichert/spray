8ef5b09c595a0ab3763b1596cb0c7ae1d16f6829
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2012-12-27 10:07:59 +0100

    finally resolve: http://code.google.com/a/eclipselabs.org/p/spray/issues/detail?id=224

--- plugins/org.eclipselabs.spray.shapes.ui/src/org/eclipselabs/spray/shapes/ui/outline/ShapeOutlinePage.java
+++ plugins/org.eclipselabs.spray.shapes.ui/src/org/eclipselabs/spray/shapes/ui/outline/ShapeOutlinePage.java
@@ -1,62 +1,7 @@
 package org.eclipselabs.spray.shapes.ui.outline;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Collection;
-
-import org.apache.log4j.Logger;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.jface.viewers.StructuredSelection;
-import org.eclipse.jface.viewers.TreeViewer;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.browser.Browser;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.xtext.ui.editor.outline.IOutlineNode;
 import org.eclipse.xtext.ui.editor.outline.impl.OutlinePage;
-import org.eclipse.xtext.ui.util.DisplayRunHelper;
-import org.eclipselabs.spray.shapes.ui.internal.ShapeActivator;
-
-import com.google.common.collect.Iterables;
 
 public class ShapeOutlinePage extends OutlinePage {
-    private static final Logger LOG = Logger.getLogger(ShapeOutlinePage.class);
-    private Browser             browser;
-    private IPath               stateLocation;
-
-    @Override
-    public void createControl(Composite parent) {
-        browser = new Browser(parent, SWT.NORMAL);
-        stateLocation = ShapeActivator.getInstance().getStateLocation().append("preview.svg");
-        if (!stateLocation.toFile().exists()) {
-            try {
-                FileWriter fw = new FileWriter(stateLocation.toFile());
-                fw.append("");
-                fw.flush();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        super.createControl(parent);
-    }
-
-    protected void refreshViewer(final IOutlineNode rootNode, final Collection<IOutlineNode> nodesToBeExpanded, final Collection<IOutlineNode> selectedNodes) {
-        DisplayRunHelper.runAsyncInDisplayThread(new Runnable() {
-            public void run() {
-                try {
-                    TreeViewer treeViewer = getTreeViewer();
-                    if (!treeViewer.getTree().isDisposed()) {
-                        treeViewer.setInput(rootNode);
-                        treeViewer.expandToLevel(1);
-                        treeViewer.setExpandedElements(Iterables.toArray(nodesToBeExpanded, IOutlineNode.class));
-                        treeViewer.setSelection(new StructuredSelection(Iterables.toArray(selectedNodes, IOutlineNode.class)));
-                        treeUpdated();
-                    }
-                    browser.setUrl(stateLocation.toFile().toURL().toString());
-                } catch (Throwable t) {
-                    LOG.error("Error refreshing outline", t);
-                }
-            }
-        });
-    }
 
 }
