277860eb072d629b2e5e9e7f505878b6fb1652be
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2013-07-19 16:42:01 +0200

    continued migrating XPECT tests

--- tests/org.eclipselabs.spray.styles.tests/src/org/eclipselabs/spray/styles/tests/StyleFormatterTest.java
+++ tests/org.eclipselabs.spray.styles.tests/src/org/eclipselabs/spray/styles/tests/StyleFormatterTest.java
@@ -1,53 +1,53 @@
 package org.eclipselabs.spray.styles.tests;
 
-import org.eclipse.xtext.formatting.INodeModelFormatter;
-import org.eclipse.xtext.formatting.INodeModelFormatter.IFormattedRegion;
-import org.eclipse.xtext.junit4.InjectWith;
-import org.eclipse.xtext.junit4.parameterized.InjectParameter;
-import org.eclipse.xtext.junit4.parameterized.ParameterSyntax;
-import org.eclipse.xtext.junit4.parameterized.ParameterizedXtextRunner;
-import org.eclipse.xtext.junit4.parameterized.ResourceURIs;
-import org.eclipse.xtext.junit4.parameterized.XpectString;
-import org.eclipse.xtext.junit4.validation.AbstractValidatorTester;
-import org.eclipse.xtext.nodemodel.ICompositeNode;
-import org.eclipse.xtext.resource.XtextResource;
-import org.eclipselabs.spray.styles.tests.util.StyleTestsInjectorProvider;
-import org.junit.runner.RunWith;
-
 import javax.inject.Inject;
 
-@RunWith(ParameterizedXtextRunner.class)
-@InjectWith(StyleTestsInjectorProvider.class)
-@ResourceURIs(baseDir = "model/formatter", fileExtensions = "style")
-public class StyleFormatterTest extends AbstractValidatorTester {
+import org.eclipse.xtext.formatting.INodeModelFormatter;
+import org.eclipse.xtext.formatting.INodeModelFormatter.IFormattedRegion;
+import org.eclipse.xtext.nodemodel.ICompositeNode;
+import org.eclipse.xtext.resource.XtextResource;
+import org.junit.runner.RunWith;
+import org.xpect.expectation.IStringExpectation;
+import org.xpect.expectation.StringExpectation;
+import org.xpect.parameter.ParameterParser;
+import org.xpect.runner.Xpect;
+import org.xpect.runner.XpectRunner;
+import org.xpect.runner.XpectTestFiles;
+import org.xpect.runner.XpectTestFiles.FileRoot;
+import org.xpect.setup.XpectSetup;
+import org.xpect.xtext.lib.setup.ThisOffset;
+import org.xpect.xtext.lib.setup.ThisResource;
+import org.xpect.xtext.lib.setup.XtextStandaloneSetup;
 
-	@InjectParameter
-	protected XtextResource resource;
+@RunWith(XpectRunner.class)
+@XpectTestFiles(relativeTo = FileRoot.CURRENT, baseDir = "model/formatter", fileExtensions = "style")
+@XpectSetup({ XtextStandaloneSetup.class })
+public class StyleFormatterTest {
 
 	@Inject
 	protected INodeModelFormatter formatter;
 
-	@InjectParameter
-	protected int offset;
-
-	@InjectParameter
-	protected int to;
-
-	@XpectString(whitespaceSensitive = true)
-	@ParameterSyntax("('from' offset=OFFSET)? 'to' to=OFFSET")
-	public String formatted() {
+	@ParameterParser(syntax = "('from' offset=OFFSET)? 'to' to=OFFSET")
+	@Xpect
+	public void formatted(
+			@StringExpectation(whitespaceSensitive = true) IStringExpectation expectation,
+			@ThisResource XtextResource resource, @ThisOffset int offset,
+			@ThisOffset int to) {
 		ICompositeNode rootNode = resource.getParseResult().getRootNode();
 		IFormattedRegion r = null;
-		if(offset >= 0 && to > offset) {
+		if (offset >= 0 && to > offset) {
 			r = formatter.format(rootNode, offset, to - offset);
 		} else {
-			r = formatter.format(rootNode, rootNode.getOffset(), rootNode.getTotalLength());	
+			r = formatter.format(rootNode, rootNode.getOffset(),
+					rootNode.getTotalLength());
 		}
-		return r.getFormattedText().replaceAll("\\r\\b", "\n") + getEnding();
+		String formatted = r.getFormattedText().replaceAll("\\r\\b", "\n")
+				+ getEnding();
+		expectation.assertEquals(formatted);
 	}
-	
+
 	private String getEnding() {
 		String ls = System.getProperty("line.separator");
 		return "\r\n".equals(ls) ? "\r" : "";
-	}	
+	}
 }
