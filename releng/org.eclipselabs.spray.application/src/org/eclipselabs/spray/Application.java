423b357187bb88fb69f9bb354726314bbdc45120
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2013-11-27 00:51:23 +0100

    preparing product project

---
+++ releng/org.eclipselabs.spray.application/src/org/eclipselabs/spray/Application.java
@@ -0,0 +1,19 @@
+package org.eclipselabs.spray;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+public class Application implements IApplication {
+
+	@Override
+	public Object start(IApplicationContext context) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+}
