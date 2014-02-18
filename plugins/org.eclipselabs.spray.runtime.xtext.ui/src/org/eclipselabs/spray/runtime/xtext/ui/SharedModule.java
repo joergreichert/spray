3f72006d793c8415b4b08bed8372d40ef2236048
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2014-02-18 09:56:20 +0100

    unbind shared module as not directly supported in Xtext 2.5.2 anymore, and the issue with linked folders should be already fixed in the current implementation of JdtToBeBuildComputer

--- plugins/org.eclipselabs.spray.runtime.xtext.ui/src/org/eclipselabs/spray/runtime/xtext/ui/SharedModule.java
+++ plugins/org.eclipselabs.spray.runtime.xtext.ui/src/org/eclipselabs/spray/runtime/xtext/ui/SharedModule.java
@@ -11,6 +11,7 @@
 package org.eclipselabs.spray.runtime.xtext.ui;
 
 import org.eclipse.xtext.builder.impl.ToBeBuiltComputer;
+import org.eclipse.xtext.common.types.access.jdt.TypeURIHelper;
 import org.eclipselabs.spray.runtime.xtext.ui.builder.JdtToBeBuiltComputerExt;
 
 import com.google.inject.AbstractModule;
@@ -20,5 +21,6 @@ public class SharedModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(ToBeBuiltComputer.class).to(JdtToBeBuiltComputerExt.class);
+        bind(TypeURIHelper.class);
     }
 }
